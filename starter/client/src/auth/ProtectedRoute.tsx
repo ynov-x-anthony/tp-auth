e98343bdcc1f49361c4b721100b2a54c7f988f05
basttouuu <hug0.paulier83@gmail.com>
2026-09-11 16:48:35 +0200

    Fin du TP

--- starter/client/src/auth/ProtectedRoute.tsx
+++ starter/client/src/auth/ProtectedRoute.tsx
@@ -15,20 +15,28 @@ import { useAuth } from './AuthContext';
 
 export function ProtectedRoute() {
   const { status } = useAuth();
+  
+  if (status === 'loading'){
+    return <p className="p-8 text-center">Chargement…</p>;
 
-  // ┌──────────────────────────────── TODO 2 ────────────────────────────────┐
-  // │ Gère les 3 valeurs possibles de `status` :                             │
+  }else if (status === 'anonymous' ){
+    return <Navigate to="/login" replace />;
+
+  }
+
+  // ┌──────────────────────────────── TODO 2 ───────────────────────────────┐
+  // │ Gère les 3 valeurs possibles de `status` :                            │
   // │                                                                       │
-  // │   • 'loading'       → on ne sait pas encore (le /auth/me est en cours).│
-  // │                       return <p className="p-8 text-center">Chargement…</p>;
+  // │   • 'loading'       → on ne sait pas encore (le /auth/me est en cours)│
+  // │                return <p className="p-8 text-center">Chargement…</p>; │
   // │                                                                       │
-  // │   • 'anonymous'     → pas connecté → renvoyer vers la page de login :  │
-  // │                       return <Navigate to="/login" replace />;         │
+  // │   • 'anonymous'     → pas connecté → renvoyer vers la page de login : │
+  // │                       return <Navigate to="/login" replace />;        │
   // │                                                                       │
-  // │   • 'authenticated' → OK → afficher la route enfant :                  │
-  // │                       return <Outlet />;                               │
+  // │   • 'authenticated' → OK → afficher la route enfant :                 │
+  // │                       return <Outlet />;                              │
   // └───────────────────────────────────────────────────────────────────────┘
 
-  console.log('[ProtectedRoute] status =', status); // repère utile - à retirer une fois corrigé
+  // console.log('[ProtectedRoute] status =', status); // repère utile - à retirer une fois corrigé
   return <Outlet />;
 }
