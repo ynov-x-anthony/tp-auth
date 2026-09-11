81df60f658825cd927ee4cafd9fdaa12d3be6e98
Lisa Payan <lisa.payan@ynov.com>
2026-09-11 16:40:11 +0200

    feat: todo3

--- starter/client/src/pages/LoginPage.tsx
+++ starter/client/src/pages/LoginPage.tsx
@@ -33,7 +33,17 @@ export function LoginPage() {
     // │    }                                                                   │
     // └───────────────────────────────────────────────────────────────────────┘
 
-    setError('TODO 3 : handleSubmit n’est pas encore implémenté.');
+    setError(null);
+    setSubmitting(true);
+
+    try {
+      await login(email, password);
+      navigate('/profile');
+    } catch (err) {
+      setError(err instanceof ApiError ? err.message : 'Erreur');
+    } finally {
+      setSubmitting(false); 
+    }
   }
 
   return (
